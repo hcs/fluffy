a7b04cafcda282e0571e4baec5a9b0c2adf585a5
Carl Jackson <carl@avtok.com>
2012-10-11 01:40:42 -0400

    Refactor things out into lib/
    Also, remove some silly debugging statements

--- index.js
+++ index.js
@@ -1,89 +1,21 @@
 var http = require('http');
-var domain = require('domain');
-var url = require('url');
 
-var forbidden_header = function(string) {
-  // We reserve the entire "fluffy-" prefix for ourselves
-  var prefix = "fluffy-";
-  if (string.substring(0, prefix.length) == prefix) {
-    return true;
-  }
+var util = require('lib/util');
 
-  // We also blacklist some other headers with special meanings
-  return ![
-    'x-forwarded-for',
-    'x-real-ip'
-  ].every(function(blacklisted) {
-    return string != blacklisted;
-  });
-};
+// Handlers
+var sanitize = require('lib/sanitize');
+var authenticate = require('lib/authenticate');
+var dispatch = require('lib/dispatch');
+var force_login = require('lib/force_login');
 
-// Sanitize headers we don't want forwared on.
-var sanitize = function(next, req, res) {
-  console.log(req.headers);
-  var headers = {};
-
-  for (var prop in req.headers) {
-    console.log(prop);
-    if (req.headers.hasOwnProperty(prop) && !forbidden_header(prop)) {
-      headers[prop] = req.headers[prop];
-    }
-  }
-
-  Object.freeze(headers);
-  req.headers = headers;
-
-  next(req, res);
-};
-
-var authenticate = function(next, req, res) {
-  next(req, res);
-};
-
-var dispatch = function(next, req, res) {
-  next(req, res);
-};
-
-var force_login = function(next, req, res) {
-  next(req, res);
-};
-
-// Utility function for closure-converting a chain of request processors
-var req_chain = function() {
-  var handler = function() {
-    throw new Error('Unknown routing error');
-  };
-  for (var i = arguments.length - 1; i >= 0; i--) {
-    handler = (function(fn, handler) {
-      return fn.bind(null, handler);
-    })(arguments[i], handler);
-  }
-  return function(req, res) {
-    // Error domain for this request. This greatly simplifies asynchronous error
-    // handling and ensuring we always send a response.
-    var d = domain.create();
-    d.on('error', function(e) {
-      // TODO: this should probably email someone or something
-      console.log(e.stack);
-      res.writeHead(500, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({
-        'error': e.message
-      }, undefined, 2) + '\n');
-    });
-
-    return d.run(function() {
-      handler(req, res);
-    });
-  };
-};
 
 var fluffy = http.createServer(
-  req_chain(sanitize, dispatch, force_login)
+  util.req_chain(sanitize, dispatch, force_login)
 );
 fluffy.listen(8001);
 /*
 var fluffys = https.createServer(
-  req_chain(sanitize, authenticate, dispatch, force_login)
+  util.req_chain(sanitize, authenticate, dispatch, force_login)
 );
 fluffy.listen(443);
 */
