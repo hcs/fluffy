76a82e01930ad3eb3425d3ff60a2e8544cda9859
Carl Jackson <carl@avtok.com>
2012-10-11 01:50:23 -0400

    Use environment variables for configuration
    Currently, we only fetch the port numbers from environment variables, with some
    reasonable defaults for development.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var http = require('http');
 
+// Fluffy is configured via environment variables. Let's set some defaults,
+// which will be especially default when developing.
+var env = process.env;
+env['FLUFFY_HTTP_PORT'] = env['FLUFFY_HTTP_PORT'] || '8001';
+env['FLUFFY_HTTPS_PORT'] = env['FLUFFY_HTTPS_PORT'] || '8002';
+
 var util = require('lib/util');
 
 // Handlers
@@ -12,10 +18,10 @@ var force_login = require('lib/force_login');
 var fluffy = http.createServer(
   util.req_chain(sanitize, dispatch, force_login)
 );
-fluffy.listen(8001);
+fluffy.listen(Number(env['FLUFFY_HTTP_PORT']));
 /*
 var fluffys = https.createServer(
   util.req_chain(sanitize, authenticate, dispatch, force_login)
 );
-fluffy.listen(443);
+fluffy.listen(Number(env['FLUFFY_HTTPS_PORT]));
 */
