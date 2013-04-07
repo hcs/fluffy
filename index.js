38e8930977925a57977d3a52741155fdd7720758
Salvatore Rinchiera <srinchiera@college.harvard.edu>
2013-04-07 13:33:04 -0400

    Commenting and added permissions.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var env = process.env;
 env['FLUFFY_HTTP_PORT'] = env['FLUFFY_HTTP_PORT'] || '8001';
 env['FLUFFY_HTTPS_PORT'] = env['FLUFFY_HTTPS_PORT'] || '8002';
 
-var util = require('lib/util');
+var util = require('./lib/util');
 
 // Handlers
-var sanitize = require('lib/sanitize');
-var authenticate = require('lib/authenticate');
-var dispatch = require('lib/dispatch');
-var force_login = require('lib/force_login');
+var sanitize = require('./lib/sanitize');
+var authenticate = require('./lib/authenticate');
+var dispatch = require('./lib/dispatch');
+var force_login = require('./lib/force_login');
 
 
 var fluffy = http.createServer(
